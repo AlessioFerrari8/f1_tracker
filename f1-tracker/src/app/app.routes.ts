40199209c15dd8c16c42704a57f6f814f2526107
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2026-02-10 19:02:46 +0100

    Feat: add routes to page

--- f1-tracker/src/app/app.routes.ts
+++ f1-tracker/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Calendar } from './pages/calendar/calendar';
+import { Drivers } from './pages/drivers/drivers';
+import { Race } from './pages/race/race';
+import { Standings } from './pages/standings/standings';
+import { Teams } from './pages/teams/teams';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: Home },
+  { path: 'calendar', component: Calendar },
+  { path: 'drivers', component: Drivers },
+  { path: 'race', component: Race },
+  { path: 'standings', component: Standings },
+  { path: 'teams', component: Teams },
+  { path: '**', redirectTo: 'home' }
+];
