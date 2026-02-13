d69e411854b212fde8409236f3de579cae46fcf9
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2026-02-13 20:13:10 +0100

    Feat: add intial content

--- f1-tracker/src/app/formula1-service.ts
+++ f1-tracker/src/app/formula1-service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Formula1Service {
-  
+  public getCalendar(): Observable<null> {
+    return null;
+  }
 }
