7fb67fd55743ed166c697ce387acdf9d3b7e1a9e
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2026-02-14 18:44:52 +0100

    Feat: start implementation of first method in service with research and tests

--- f1-tracker/src/app/formula1-service.ts
+++ f1-tracker/src/app/formula1-service.ts
@@ -1,11 +1,24 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { IMeetings } from './interfaces/i-mettings';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Formula1Service {
-  public getCalendar(): Observable<null> {
+  // client http per gestire richieste
+  private httpClient: HttpClient = inject(HttpClient);
+
+  // all urls I'll use
+  private urls: string[] = [
+    'https://api.openf1.org/v1/meetings?year=2026',
+
+  ]
+
+  public getCalendar(): Observable<IMeetings | null> {
+    let url = this.urls[0];
     return null;
+
   }
 }
