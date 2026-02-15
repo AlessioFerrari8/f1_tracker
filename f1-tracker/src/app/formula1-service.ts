3892ab5d6148efc639854244a984157d0e6cd5a9
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2026-02-15 14:30:00 +0100

    Feat: add method to gather a driver infos

--- f1-tracker/src/app/formula1-service.ts
+++ f1-tracker/src/app/formula1-service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { catchError, Observable, of } from 'rxjs';
 import { IMeetings } from './interfaces/i-mettings';
+import { IDriver } from './interfaces/i-driver';
 
 @Injectable({
   providedIn: 'root',
@@ -13,7 +14,7 @@ export class Formula1Service {
   // all urls I'll use
   private urls: string[] = [
     'https://api.openf1.org/v1/meetings?year=2026',
-
+    'https://api.openf1.org/v1/drivers?driver_number=1&session_key=9158',
   ]
 
   public getCalendar(): Observable<IMeetings> {
@@ -22,5 +23,13 @@ export class Formula1Service {
       catchError(() => of([]))
     );
   }
-  
+
+  public getDriver(driver_number: number): Observable<IDriver> {
+    // 'https://api.openf1.org/v1/drivers?driver_number=1&session_key=9158',
+    let url = this.urls[1];
+    return this.httpClient.get<IDriver>(url).pipe(
+      catchError(() => of())
+    );
+  }
+
 }
